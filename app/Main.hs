286d3558b1bc76b025ca9329149c757b4c22f680
Pawel Rusin <rusin.paw@gmail.com>
2017-03-22 23:45:39 +0900

    Implement flag picking (#1)

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ data GameState = GameState { deck :: Deck, player1 :: Player, player2 :: Player 
 type Hand = [Card]
 data Player = Player { hand :: Hand }
 data Card = Card deriving (Show)
-data Flag = Flag
+data Flag = One | Two | Three | Four | Five | Six | Seven | Eight | Nine deriving Enum
 data PlayerNumber = Player1 | Player2
 
 exit :: IO ()
@@ -42,9 +42,23 @@ getPlayer playerNumber gameState = case playerNumber of
                                         Player1 -> player1 gameState
                                         Player2 -> player2 gameState 
 
-
 chooseFlag :: PlayerNumber -> GameState -> IO Flag
-chooseFlag = undefined
+chooseFlag playerNumber gameState = go
+    where
+        go = getFlag >>= maybe go return
+        getFlag = flagFromString <$> ask "Please pick aflag"
+
+flagFromString :: String -> Maybe Flag
+flagFromString "1" = Just One
+flagFromString "2" = Just Two
+flagFromString "3" = Just Three
+flagFromString "4" = Just Four
+flagFromString "5" = Just Five
+flagFromString "6" = Just Six
+flagFromString "7" = Just Seven
+flagFromString "8" = Just Eight
+flagFromString "9" = Just Nine
+flagFromString _ = Nothing
 
 updateGame :: PlayerNumber -> Card -> Flag -> GameState -> GameState
 updateGame = undefined
