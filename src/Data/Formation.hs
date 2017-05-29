6f75b7e89f76f991dd1f9c1f06990157ffe76dc7
Pawel Rusin <pawel-rusin@cookpad.com>
2017-05-29 23:34:44 +0900

    add first Formations draft

---
+++ src/Data/Formation.hs
@@ -0,0 +1,15 @@
+module Data.Formation where
+
+data Formation = Formation {formationType :: FormationType,
+                            formationValue :: Int
+                           } 
+
+data FormationType = Host | Skirmish | Battalion | Phalanx | Wedge
+data Card = Card {color :: Color,
+                  value :: Int
+                  }
+
+data Color = Red | Blue | Purpule | Pink | Green | Yellow deriving (Show, Eq)
+
+isWedge :: Formation -> Bool
+isWedge formation = True
---
+++ test/Data/FormationSpec.hs
@@ -0,0 +1,17 @@
+module Data.FormationSpec (main, spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+
+import Data.Formation
+
+-- `main` is here so that this module can be run from GHCi on its own.  It is
+-- not needed for automatic spec discovery.
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "formation" $ do
+    it "Wedge is three cards with the same color and value" $ do
+      isWedge Formation(Card(Red 3, Red 4, Red 5)) `shouldBe` True
